f80700f9348c715783a96d351bfb9301f1844217
Anup <anuprai42128@gmail.com>
2026-05-04 01:05:03 +0530

    REMOVE PWA

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Force unregister any lingering PWA service workers
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+      console.log('Unregistered old PWA service worker');
+    }
+  });
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
